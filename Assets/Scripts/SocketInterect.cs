66bc71b62760b3b5cd5e2527b0a844a477edba17
ttruty <ttruty@gmail.com>
2020-01-20 11:48:17 -0600

    First commit

---
+++ Assets/Scripts/SocketInterect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocketInterect : MonoBehaviour
+{
+    public GameObject door;
+    private Animator anim;
+    void Start()
+    {
+        anim = door.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision:");
+        Debug.Log(other.name);
+        if (other.name == "key")
+        {
+            anim.SetTrigger("DoorOpen");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Collision:");
+        Debug.Log(other.name);
+        if (other.name == "key")
+        {
+            anim.SetTrigger("DoorClosed");
+        }
+    }
+}
